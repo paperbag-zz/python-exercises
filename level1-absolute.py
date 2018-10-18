1d33819e5e5c5793dd524b989d88845b2f0f2ff9
Arnoldas <paperbag.a@gmail.com>
2018-10-18 19:42:15 +0100

    new level1 problem solutions

---
+++ level1-absolute.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+def absolute(n):
+    return((abs(100 - n) <= 10) or (abs(200 - n) <= 10))
---
+++ reverse-sentence.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+def reverse(text):
+    text_split = text.split()
+    text_split.reverse()
+    return " ".join(text_split)
+
+
+
+
+    
