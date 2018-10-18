644777cef85c6ffc11a632de8dbaec08b11088fc
Arnold <paperbag.a@gmail.com>
2018-10-18 19:46:50 +0100

    Update 1-ex.py

--- 1-ex.py
+++ 1-ex.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+#
+#warmup 1 challenge for udemy python bootcamp course
+#
+## LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers *if* both numbers are even, but returns the greater if one or both numbers are odd\n"
 def lesser(a,b):
     if a % 2 == 0 and b % 2 == 0:
         return min(a,b)
